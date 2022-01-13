dd6986c8f34f6cafc9475010eb125456f3e03f6e
JordanHue <97678561+JordanHue@users.noreply.github.com>
2022-01-13 21:50:31 +0700

    pertemuan-01

---
+++ pertemuan-01/project Jordan/src/project/jordan/ProjectJordan.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package project.jordan;
+
+/**
+ *
+ * @author user
+ */
+public class ProjectJordan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+System.out.println("hello Dunia Satu Keluarga");
+        // TODO code application logic here
+    }
+    
+}
